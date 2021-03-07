997e3ba812ea00ca763d5b047e844b0026c92d89
MiguelED <migueled2695@gmail.com>
2021-03-07 17:25:51 -0600

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre      : string      = 'Capitán América';
+  nombre2     :string       ='MiGuEL EDUardO SilVAno';
+  arreglo     : number[]    =[1,2,3,4,5,6,7,8,9,10];
+  PI          : number      =Math.PI;
+  porcentaje  :number       =0.234;
+  salario     :number       =1234.5;
+  fecha       :Date         = new Date();
+  idioma      :string       ='es';
+  video       :string       ='https://www.youtube.com/embed/WNxH3Qnx0LQ';
+  activar     :boolean      =true;
+  
+  valorpromesa=new Promise<string>((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('LLego la data');
+      //reject('error');
+    },5500);
+  });
+
+  heroe={
+    nombre:   'logan',
+    clave:    'wolverine',
+    edad:      500,
+    direccion:{
+      calle:  'primera',
+      casa:    20
+    }
+  }
+  
+  idiomaButton(idioma:string){
+    this.idioma=idioma;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { PasswordPipe } from './pipes/password.pipe';
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    PasswordPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:LOCALE_ID,
+      useValue:'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
